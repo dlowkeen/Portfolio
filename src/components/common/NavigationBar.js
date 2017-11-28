24fdd662c217e6b6153923a8b47e3a98f9c1430c
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2017-11-27 16:00:02 -0800

    added navbar

---
+++ src/components/common/NavigationBar.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+
+class NavigationBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Container>
+          <Navbar className="fixed-top" toggleable id="navigation">
+            <NavbarToggler right onClick={this.toggle} />
+            <NavbarBrand href="/" className="page-title">
+              Donovan Lowkeen
+            </NavbarBrand>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink className="page-title" href="#about">
+                    About
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="page-title" href="#skills">
+                    Skills
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="page-title" href="#projects">
+                    Projects
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="page-title" href="#contact">
+                    Contact
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default NavigationBar;
