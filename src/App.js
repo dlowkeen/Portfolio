d53b243816a6420cbebb0d9cc1e0335abe523be9
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-01-20 09:41:44 -0800

    cleaning up file structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import NavigationBar from './components/common/NavigationBar';
 import Landing from './components/Landing';
 import AboutMe from './components/AboutMe';
